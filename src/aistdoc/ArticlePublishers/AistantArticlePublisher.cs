8711dc93d9667abf5876e35659f2c1217a1430e0
Sergiy Korzh <sergey.korzh@live.com>
2021-04-06 11:00:07 +0300

    Add ArticlePublishers

---
+++ src/aistdoc/ArticlePublishers/AistantArticlePublisher.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+using Aistant.KbService;
+
+namespace aistdoc
+{
+    internal class AistantArticlePublisher : IArticlePublisher 
+    {
+        private readonly AistantKbService _service;
+
+        public AistantArticlePublisher(AistantSettings settings, ILogger logger ) 
+        {
+            _service = new AistantKbService(settings, logger);
+        }
+
+        public bool PublishArticle(ArticlePublishModel model)
+        {
+            return _service.UploadArticleAsync(model.SectionUri, model.SectionTitle, model.ArticleUri, model.ArticleTitle, model.ArticleBody, model.ArticleExcerpt, model.IsSection).Result;
+        }
+    }
+}
---
+++ src/aistdoc/ArticlePublishers/FileArticlePublisher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+using Microsoft.Extensions.Logging;
+
+namespace aistdoc
+{
+
+    public class FileArticlePublisher : IArticlePublisher 
+    {
+        private readonly string _outputFolder;
+        private readonly ILogger _logger;
+
+        public FileArticlePublisher(string outputFolder, ILogger logger)
+        {
+            _outputFolder = outputFolder;
+            _logger = logger;
+
+            if( Directory.Exists(_outputFolder)) {
+                Directory.Delete(_outputFolder, true);
+                Directory.CreateDirectory(_outputFolder);
+            }
+        }
+
+
+        public bool PublishArticle(ArticlePublishModel model) 
+        {
+            try {        
+                var articleDirectory = Path.Combine(_outputFolder, MakeFileNameFromTitle(model.SectionTitle));
+                Directory.CreateDirectory(articleDirectory);
+
+                var articleTitleAndExcerpt = "## " + model.ArticleTitle + "\n";
+                articleTitleAndExcerpt += model.ArticleExcerpt + "\n";
+
+                //section index file
+                File.AppendAllText(Path.Combine(articleDirectory, "$index.md"), articleTitleAndExcerpt);
+
+                //article file
+                var filepath = Path.Combine(articleDirectory, MakeFileNameFromTitle(model.ArticleTitle)) + ".md";
+                File.WriteAllText(filepath, model.ArticleBody);
+
+                _logger.LogInformation("Article was published. Path: " + filepath);
+            }
+            catch (Exception ex) {
+                _logger.LogError($"ERROR on publishing [{model.ArticleTitle}]:" + ex.Message);
+                return false;
+            }
+           
+            return true;
+        }
+
+        private Regex _rightRegex = new Regex("[\\~#%&*{}/:<>?|\"-]");
+
+        private string MakeFileNameFromTitle(string title) {
+            return _rightRegex.Replace(title, "");
+        }
+
+    }  
+}
